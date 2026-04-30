fecc7c224f470e1d10c5c9827e0ec357706f00ed
masterkris <krrishganesh123@gmail.com>
2026-04-29 22:27:39 -0500

    Modifications

--- bots/part2.s
+++ bots/part2.s
@@ -44,6 +44,9 @@ MMIO_STATUS             = 0xffff204c
 
 .data
 puzzle_received: .word 0
+received_coordinate: .word 0 # for our coordinate logic
+target_x: .word 0
+target_y: .word 0
 bunnies: .space 484 # allocate
 # If you want, you can use the following to detect if a bonk has happened.
 has_bonked: .byte 0
@@ -103,20 +106,7 @@ main:
      sw $0, puzzle_received  # after solving, clear and request new immediately
      la $t0, puzzle_data
      sw $t0, REQUEST_PUZZLE
-
-coordinate_loop:
-    lw $t0, coordinate_received   # (x,y) received from puzzle interrupt handler
-    beq $t0, $0, cordinate_loop    
-    # turn bot with absolute angle toward target
-    # calculate cycles until target reached
-    # request timer interrupt for that many cycles
-    # set velocity to 10
-    if bot has absolute angle:
-        sw $t2, ANGLE_CONTROL # absolute angle
-    
-    calculate num. cycles until target reached and store in $t3
-        sw $t3, TIMER  # request timer interrupt for that many cycles
-        sw $t2, VELOCITY # set velocity to 10
+     j bunny_loop
 
 bunny_loop:
     la $s4, bunnies        
@@ -145,129 +135,45 @@ find_bunny: # make sure bunny won't hop in next 100,000 cycles
     add $t4, $t4, 1   # index increment
     blt $t4, $s5, find_bunny  # loop to find next bunny and check
     addi $t0, $s4, 4   # use bunnies[0] if no bunny valid
-    j found_bunny
 
 found_bunny:
     lw $s0, 0($t0)
     lw $s1, 4($t0)
 
-# dealing with x-coordinate
-x_loop:
-    lw $t8, puzzle_received
-    beq $t8, $0, cont_x_loop # modify
+    jal move_to_xy
 
-cont_x_loop:
-    lw $t1, BOT_X # curr. x
-    beq $t1, $s0, stop_x # has bot reached bunny?
-
-    blt $t1, $s0, move_right # current position < bunny's position
-    li $t2, 180  # if not, then we have to go other way
-    j move_x
-
-move_right:
-    li $t2, 0
-
-move_x:
-    # set angle and turn it
-    sw $t2, ANGLE  
-    li $t2, 1
-    sw $t2, ANGLE_CONTROL
-    li $t2, 10
-    sw $t2, VELOCITY
-    j x_loop # until reach bunny's x-coordinate
-
-stop_x:
-    sw $0, VELOCITY   # stop if we have reached
-
-# dealing with y-coordinate
-y_loop:
-    lw $t1, BOT_Y # curr. y
-    beq $t1, $s1, stop_y # has bot reached bunny?
-
-    blt $t1, $s1, move_south # current position < bunny's position
-    li $t2, 270  # if not, then we have to go other way
-    j move_y
-
-move_south:
-    li $t2, 90
-
-move_y:
-    # set angle and turn it
-    sw $t2, ANGLE  
-    li $t2, 1
-    sw $t2, ANGLE_CONTROL
-    li $t2, 10
-    sw $t2, VELOCITY
-    j y_loop # until reach bunny's x-coordinate
-
-stop_y:
-    sw $0, VELOCITY   # stop if we have reached
     li $t1, 1
-    sw $t1, CATCH_BUNNY
+    sw $t1, CATCH_BUNNY # catch bunny after reaching
 
-
-
-# playpen logic
+deliver_to_playpen:
 
     lw $t0, PLAYPEN_LOCATION
-    srl $s2, $t0, 16 # for playpen X (upper 16 bits)
-    sll $s3, $t0, 16 
-    srl $s3, $s3, 16  # for playpen Y (lower 16 bits)
+    srl $a0, $t0, 16 # for playpen X (upper 16 bits)
+    sll $a1, $t0, 16  # remove upper bits
+    srl $a1, $s3, 16  # for playpen Y (lower 16 bits)
 
-# horizontal movement
-playpen_x:
+    # now move diagonally to playpen since we can move in both x and y at the same time
+    jal move_to_xy
 
-    lw $t1, BOT_X   # get X pos.
-    beq $t1, $s2, stop_x_and_move_y  # if X reached, move to Y
-    blt $t1, $s2, go_right  #  current position < target position
-    li $t2, 180 # else go left
-    j deliver_x
+    lw $t1, NUM_BUNNIES_CARRIED
+    sw $t1, PUT_BUNNIES_IN_PLAYPEN 
 
-go_right:
-    li $t2, 0
+    j first_loop 
 
-deliver_x:
-    sw $t2, ANGLE  
-    li $t3, 1
-    sw $t3, ANGLE_CONTROL # absolute angle
-    li $t3, 10
-    sw $t3, VELOCITY
-    j playpen_x # until we match x-coord.
+move_to_xy:
+# input (x,y) --> $a0, $a1
+# output: bot moves to (x,y)
 
-stop_x_and_move_y:
-    sw $0, VELOCITY
+# need a xy_loop, to handle x and y as we move diagonally?
+# need to handle diagonal movement for up left, up right, down left, down right
+# handle relative to target
+# absolute angle move
+# TO-DO
 
-
-# vertical playpen
-playpen_y:
-
-    lw $t1, BOT_Y   # get X pos.
-    beq $t1, $s3, stop_y_and_deliver  # if X reached, move to Y
-    blt $t1, $s3, go_south  #  current position < target position
-    li $t2, 270 # else go up
-    j deliver_y
-
-go_south:
-    li $t2, 90
-
-deliver_y:
-    sw $t2, ANGLE  
-    li $t3, 1
-    sw $t3, ANGLE_CONTROL # absolute angle
-    li $t3, 10
-    sw $t3, VELOCITY
-    j playpen_y # until we match y-coord.
-
-stop_y_and_deliver:
-    sw $0, VELOCITY
-
-    lw $t1, NUM_BUNNIES_CARRIED 
-    sw $t1, PUT_BUNNIES_IN_PLAYPEN
-      
-    j first_loop  
+move_xy_loop:
 
 solve_puzzle:
-    sub $sp, $sp, 4  # allocate
+    sub $sp, $sp, 8  # allocate
     sw $ra, 0($sp)
     sw $s0, 4($sp)  # puzzle_num
     move $s0, $a0
@@ -293,7 +199,7 @@ solve_puzzle:
 
     lw $ra, 0($sp)   # deallocate
     lw $s0, 4($sp)
-    add $sp, $sp, 4
+    add $sp, $sp, 8
     jr $ra
 
 
